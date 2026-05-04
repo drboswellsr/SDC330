4fe11f5e84d97a16cf5503d30584145e85fcbeab
drboswellsr <drboswellsr@gmail.com>
2026-05-04 18:55:38 -0400

    Week 3 - Phase #3 Submission
    Implemented abstraction using abstract Firearm class, added constructors, updated access specifiers, and expanded firearm inventory project

--- Rifle.java
+++ Rifle.java
@@ -1,34 +1,15 @@
 /*
- * Name: Darrien Raines-Boswell
- * Date: April 16, 2026
- * Purpose: Derived class that extends Firearm with rifle-specific data. This file helps
- * demonstrate inheritance and polymorphism.
+ * Purpose: Rifle class extending Firearm
  */
 
 public class Rifle extends Firearm {
-    private double barrelLength;
-    private String stockType;
 
-    public Rifle(String name, String manufacturer, String caliber, String type,
-                 double barrelLength, String stockType) {
-        super(name, manufacturer, caliber, type);
-        this.barrelLength = barrelLength;
-        this.stockType = stockType;
+    public Rifle(String brand, String model, String caliber) {
+        super(brand, model, caliber);
     }
 
-    public double getBarrelLength() {
-        return barrelLength;
-    }
-
-    public String getStockType() {
-        return stockType;
-    }
-
-    // Polymorphism is demonstrated here by overriding displayDetails()
     @Override
-    public void displayDetails() {
-        super.displayDetails();
-        System.out.println("Barrel Length: " + barrelLength + " inches");
-        System.out.println("Stock Type: " + stockType);
+    public void firearmType() {
+        System.out.println("Type: Rifle");
     }
 }
