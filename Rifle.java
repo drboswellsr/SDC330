e18e3a1b5405156ea16e0339960a3dd168a5f330
drboswellsr <drboswellsr@gmail.com>
2026-05-04 20:36:04 -0400

    Update Rifle class with properties, constructor, and toString method
    Added barrel length, optic, and accessory fields.
    Implemented constructor, getters, and overridden toString method.

--- Rifle.java
+++ Rifle.java
@@ -1,15 +1,40 @@
-/*
- * Purpose: Rifle class extending Firearm
- */
+// Darrien Raines-Boswell
+// Rifle class
 
 public class Rifle extends Firearm {
+    private double barrelLength;
+    private String optic;
+    private String accessory;
 
-    public Rifle(String brand, String model, String caliber) {
-        super(brand, model, caliber);
+    public Rifle(String name, String caliber, double barrelLength, String optic, String accessory) {
+        super(name, caliber);
+        this.barrelLength = barrelLength;
+        this.optic = optic;
+        this.accessory = accessory;
+    }
+
+    public double getBarrelLength() {
+        return barrelLength;
+    }
+
+    public String getOptic() {
+        return optic;
+    }
+
+    public String getAccessory() {
+        return accessory;
     }
 
     @Override
-    public void firearmType() {
-        System.out.println("Type: Rifle");
+    public String getFirearmType() {
+        return "Rifle";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nBarrel Length: " + barrelLength + " inches" +
+                "\nOptic: " + optic +
+                "\nAccessory: " + accessory;
     }
 }
